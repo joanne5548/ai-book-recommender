146aa22a69baa43b3df7af51cf6af4611a98bb1d
joanne5548 <91037706+joanne5548@users.noreply.github.com>
2023-06-29 18:03:12 -0400

    update front page

--- frontend/src/component/SearchBar.tsx
+++ frontend/src/component/SearchBar.tsx
@@ -4,30 +4,49 @@ import { Typeahead } from 'react-bootstrap-typeahead';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
-import Titles from '../../public/title.json'
+import Books from '../../public/books.json'
+import header from '../assets/Header.png'
 
 function SearchBar() {
     const [selected, setSelected] = useState([]);
     const options = ['Harry Potter 1', 'Hunger Game 1', 'The Way of Kings', 'Animal Farm'];
     const [text, setText] = useState([]);
-    
-    const book_list = // Start Here
 
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Select your favorite books</Form.Label>
-                <Typeahead
-                    id='basic-typeahead'
-                    labelKey='name'
-                    multiple
-                    onChange={setSelected}
-                    options={options}
-                    placeholder="Click here to search"
-                    selected={selected}
-                    />
-            </Form.Group>
-        </Form>
+        <div>
+            <h1 className="mt-4 position-absolute top-0 start-50 translate-middle-x">
+                Welcome to AI Book Recommender!
+            </h1>
+
+            <Container>
+                <Row>
+                    <p class="text-center fs-5">Select your favorite books</p>
+                    <Col class="col-md-5 offset-md-3">
+                        <Form>
+                            <Form.Group>
+                                <Typeahead
+                                    id='basic-typeahead'
+                                    labelKey='name'
+                                    multiple
+                                    onChange={setSelected}
+                                    options={options}
+                                    placeholder="Click here to search"
+                                    selected={selected}
+                                    />
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                    
+                    <Col class="col-md-1">
+                        <Button>
+                            Done
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        
+        </div>
+        
     );
 };
 
