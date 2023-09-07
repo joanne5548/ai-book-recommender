c595f0779c900e842d20b75974e378f0e475b80e
Sam Gallic <samgallic@ufl.edu>
2023-09-07 15:30:59 -0400

    Changed routes for backend methods

--- backend/book.py
+++ backend/book.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_restful import Resource, Api
 import cluster
 
 app = Flask("BookAPI")
-api = Api(app)
 
-class Book(Resource):
-    def get(self, id):
-        if id == 'all':
-            return cluster.MakeBooksList()
-        intid = int(id)
-        rec = cluster.Recommend(intid, 10)
-        return rec
+@app.route('/books', methods=['GET'])
+def GetAllBooks():
+    return cluster.MakeBooksList()
 
-api.add_resource(Book, '/books/<id>')
+@app.route('/books/<id>', methods=['GET'])
+def GetBookObject(id):
+    return cluster.IDToBook(id, cluster.MakeBooksList())
+
+@app.route('/recommend/<id>', methods=['GET'])
+def ReccomendBooks(id):
+    return cluster.Recommend(id, 10)
